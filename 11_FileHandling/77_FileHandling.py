2ab48dc51223cbbc16d05573230504d67acea4ba
Vishal Pandey <victorvishal12@gmail.com>
2026-09-05 23:18:29 +0530

    File Handling

---
+++ 11_FileHandling/77_FileHandling.py
@@ -0,0 +1,17 @@
+# File Handling in Python is a way to read and write files. Python provides built-in functions to create, read, update, and delete files.
+
+# There are two types of files in Python:
+# Text files(.txt, .csv, .json, etc.) - store data in text format
+# Binary files(.jpg, .png, .pdf, etc.) - store data in binary format
+
+#syntax to open a file in Python:
+# file_object = open("filename", "mode")
+
+# Modes to open a file in Python:
+# 1. "r" - Read mode - Default mode. Opens a file for reading
+# 2. "w" - Write mode - Opens or creates a file for writing
+# 3. "a" - Append mode - Opens/creates a file for appending data
+# 4. "x" - Exclusive creation - Creates a new file, returns an error if the file exists
+# 5. "b" - Binary mode - Opens a file in binary mode
+# 6. "t" - Text mode - Opens a file in text mode (default)
+
---
+++ 11_FileHandling/78_OpenFile.py
@@ -0,0 +1,23 @@
+# Synatx
+# open('filename', 'mode')
+
+file = open(r"11_FileHandling\file.txt", "r")#r before the path is used for telling python its a raw string, otherwise it treasts \ as an escape character.
+# or use / or \\ in the path instead of \
+
+print(file.read()) #reads the entire file
+print("================================")
+# print(dir(open)) 
+# print(file.readline()) #reads the first line of the file
+#BUT RETURNS empty string, as cursor is at the end of file, need to reset cursor
+# print("================================")
+
+# print(file.readlines()) #reads all the lines of the file and returns a list of lines
+# print("================================")
+
+#file.read() reads the entire file and puts the cursor at the end of the file, so if we call file.read() or file.readline() again, it will return an empty string. 
+# to fix this we reset curson to the begining of the file using file.seek(0)
+file.seek(0) #resets the cursor to the begining of the file
+print(file.readline()) #reads the first line of the file 
+
+file.close() #closes the file/ releases the resource
+
