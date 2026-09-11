f64c1210761d7f1db6cc9590e17f150458cc3fd6
Vishal Pandey <victorvishal12@gmail.com>
2026-09-11 23:11:05 +0530

    added functionality

--- Projects/Slot Machine Game/main.py
+++ Projects/Slot Machine Game/main.py
@@ -1,14 +1,52 @@
-def deposit():
-    while True: #infinite loop
-        amount = input("How much would you like to deposit? $")
+MAX_LINES = 3 #Screaming shows CONSTANT
+MAX_BET = 100
+MIN_BET = 1
 
+def deposit():
+    while True:
+        amount = input("Enter the amount to deposit: ")
         if amount.isdigit():
             amount = int(amount)
-            if amount <= 0:
-                print("Enter Number greater than 0")
+            if amount<=0:
+                print("Enter a number greater than 0")
             else:
+                print("You deposited "+ str(amount) + "$")
                 break
         else:
-            print("Enter a number only")
+            print("Enter a number fool!")
 
-deposit()
+    return amount
+
+def getNumberOfLines():
+    while True:
+        line = input(f"Enter the number of lines to bet on (1 - {MAX_LINES}) ")
+        if line.isdigit():
+            line = int(line)
+            if 1 <= line <= MAX_LINES:
+                print(f"You selected {line} lines to bet on!")
+                break
+            else:
+                print(f"Enter a number between 1-{MAX_LINES}")
+        else:
+            print("Enter a number BRO!")
+    return line
+
+def getBet():
+    while True:
+        bet = input(f"Enter Amount to bet on (${MIN_BET} - ${MAX_BET}) :")
+        if bet.isdigit():
+            bet = int(bet)
+            if MIN_BET <= bet <= MAX_BET:
+                break
+            else:
+                print(f"Enter a number between {MIN_BET} - {MAX_BET}")
+        else:
+            print("Enter a number BRO!")
+    return bet
+
+def main():
+    balance = deposit()
+    lines = getNumberOfLines()
+    bet = getBet()
+
+main()
